5d0cf93a6d9782fb03a697fc5d709414c8cbd25f
Justin Fan <justin.fan@flightaware.com>
2019-11-21 15:19:19 -0600

    done

---
+++ vimDemo.tcl
@@ -0,0 +1,21 @@
+proc hello {arg1 arg2} {
+	set helloThere arg1
+	return $hello
+}
+set val "/usr/local"
+proc get_down {} {
+	set result "[list "hello" {what} {nested {value here}}]"
+	set res.ult_val_2 "hello2"
+	complicated_function_call
+	puts $result
+	puts $secondChange
+	return $result
+}
+
+proc calculate {words how_many} {
+	puts "hello"
+	puts $result
+
+
+	complicated_function_call
+}
