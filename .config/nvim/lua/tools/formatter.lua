2ee084b0c1ffb79654aa42862070a3782ab4272d
Justin Fan <justinfan827@gmail.com>
2023-04-07 16:51:08 -0700

    added lspsaga, tailwindcss, emmet, snippets in general

--- .config/nvim/lua/tools/formatter.lua
+++ .config/nvim/lua/tools/formatter.lua
@@ -7,6 +7,16 @@
 require("formatter").setup(
   {
     filetype = {
+      html = {
+        -- prettier
+        function()
+          return {
+            exe = "prettierd",
+            args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
+            stdin = true
+          }
+        end
+      },
       sh = {
         -- Shell Script Formatter
         function()
@@ -18,6 +28,16 @@ require("formatter").setup(
         end
       },
       yaml = {
+        -- prettier
+        function()
+          return {
+            exe = "prettier",
+            args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))},
+            stdin = true
+          }
+        end
+      },
+      typescriptreact = {
         -- prettier
         function()
           return {
