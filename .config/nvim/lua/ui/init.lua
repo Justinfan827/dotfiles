c5477e0f4d7859397ca929fa9074b04fb3cd232f
Justin Fan <justinfan827@gmail.com>
2023-07-17 11:23:11 -1000

    moved onto mason for lsp management

--- .config/nvim/lua/ui/init.lua
+++ .config/nvim/lua/ui/init.lua
@@ -1,5 +1,6 @@
 vim.o.background = "dark" -- or "light" for light mode
 require("ui.lualine")
 require("ui.bufferline")
-require("ui.colorizer")
 require("ui.indent-blankline")
+
+require("nvim-highlight-colors").setup {}
